7392e0325844d2468d80e915cb7933ca195b588f
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-08-30 12:32:37 +0200

    feat: add portapage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './dom/dom';
 export * from './dom/domUtils';
 export * from './dom/types';
+export * from './page/page';
+export * from './page/pageUtils';
+export { Portapage, InfiniteScrollLoaderOptions } from './page/types';
