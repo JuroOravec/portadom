a5cd005631049972719422ae16e9d43fc2a7298c
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-09-01 12:24:15 +0200

    refactor: remove unused utils

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,2 @@
 export type MaybePromise<T> = T | Promise<T>;
 export type MaybeArray<T> = T | T[];
-
-export type ArrVal<T extends any[] | readonly any[]> = T[number];
-
-export type PickPartial<T extends object, Keys extends keyof T> = Omit<T, Keys> &
-  Partial<Pick<T, Keys>>;
-
-export type PickRequired<T extends object, Keys extends keyof T> = Omit<T, Keys> &
-  Required<Pick<T, Keys>>;
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,13 +1,3 @@
-/** Validate correctness of a URL */
-export const validateUrl = (url: string) => {
-  try {
-    new URL(url);
-  } catch (err) {
-    (err as Error).message += `\nURL: "${url}"`;
-    throw err;
-  }
-};
-
 /** Transform relative URL paths to absolute URLs */
 export const resolveUrlPath = (urlBase: string, urlPath: string) => {
   const url = new URL(urlBase);
